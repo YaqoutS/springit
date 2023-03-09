a4d8d96bb23bfa596c60dcb9ce056a95a237e203
YaqoutS <yaqoutsalameh@gmail.com>
2023-03-09 13:04:44 +0200

    Created our entities and refactored using Project Lombok

---
+++ src/main/java/com/salameh/springit/domain/Comment.java
@@ -0,0 +1,19 @@
+package com.salameh.springit.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+
+    //link
+}
---
+++ src/main/java/com/salameh/springit/domain/Vote.java
@@ -0,0 +1,20 @@
+package com.salameh.springit.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+
+    //user
+    //link
+}
