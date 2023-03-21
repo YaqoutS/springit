633fa2ab758e9140370c802532b2dc9f28327a00
YaqoutS <yaqoutsalameh@gmail.com>
2023-03-21 16:51:22 +0200

    Custom Login Form

--- src/main/java/com/salameh/springit/security/SecurityConfiguration.java
+++ src/main/java/com/salameh/springit/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.salameh.springit.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +8,22 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
     public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
@@ -23,19 +34,36 @@ public class SecurityConfiguration {
         return http
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(EndpointRequest.to("info")).permitAll();
-                    auth.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR");
-                    auth.requestMatchers("/actuator/").hasRole("ACTUATOR");
-                    auth.requestMatchers("/link/submit").hasRole("USER");
+                    auth.requestMatchers(EndpointRequest.toAnyEndpoint()).hasAuthority("ACTUATOR");
+                    auth.requestMatchers("/actuator/").hasAuthority("ACTUATOR");
+                    auth.requestMatchers("/link/submit").hasAuthority("USER");
                     auth.requestMatchers("/link/**").permitAll();
                     auth.requestMatchers("/").permitAll();
                     auth.requestMatchers("/h2-console/**").permitAll();
                     auth.anyRequest().permitAll();
                 })
-                .formLogin(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-                .headers().disable()
-                .build();
-        //or return http.build();
+                .formLogin(form -> form.loginPage("/login").permitAll()).userDetailsService(userDetailsService)
+//                .csrf(csrf -> csrf.disable()) // To be able to see the h2 database, we need to disable csrf. But in production we need it to be enabled to prevent csrf attack.
+//                .headers().disable()
+                .build();         //or return http.build();
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user@gmail.com")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
 }
