482c6e7a8a37109a042c8fba41d35e08087a50a7
yaqout salameh <yaqoutsalameh@gmail.com>
2023-03-23 12:47:13 +0200

    Remember me and logout

--- src/main/java/com/salameh/springit/security/SecurityConfiguration.java
+++ src/main/java/com/salameh/springit/security/SecurityConfiguration.java
@@ -17,6 +17,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -31,7 +35,7 @@ public class SecurityConfiguration {
         this.userDetailsService = userDetailsService;
     }
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain configure(HttpSecurity http, RememberMeServices rememberMeServices) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(EndpointRequest.to("info")).permitAll();
@@ -43,10 +47,35 @@ public class SecurityConfiguration {
                     auth.requestMatchers("/h2-console/**").permitAll();
                     auth.anyRequest().permitAll();
                 })
-                .formLogin(form -> form.loginPage("/login").permitAll()).userDetailsService(userDetailsService)
-                .csrf(csrf -> csrf.disable()) // To be able to see the h2 database, we need to disable csrf. But in production we need it to be enabled to prevent csrf attack.
-                .headers().disable()
+                .formLogin(form -> form.loginPage("/login").permitAll().usernameParameter("email")
+                ).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .permitAll())
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .anyRequest().authenticated()
+//                )
+                .rememberMe((remember) -> remember
+                        .rememberMeServices(rememberMeServices))
+
+//                .csrf(csrf -> csrf.disable()) // To be able to see the h2 database, we need to disable csrf. But in production we need it to be enabled to prevent csrf attack.
+//                .headers().disable()
                 .build();         //or return http.build();
     }
+
+    @Bean
+    RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+        TokenBasedRememberMeServices.RememberMeTokenAlgorithm encodingAlgorithm = TokenBasedRememberMeServices.RememberMeTokenAlgorithm.SHA256;
+        String myKey = "Yaqout";
+        TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices(myKey, userDetailsService, encodingAlgorithm);
+        rememberMe.setMatchingAlgorithm(TokenBasedRememberMeServices.RememberMeTokenAlgorithm.MD5);
+        return rememberMe;
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+//        builder.userDetailsService(userDetailsService);
+//                //.passwordEncoder(passwordEncoder());
+//    }
 
 }
